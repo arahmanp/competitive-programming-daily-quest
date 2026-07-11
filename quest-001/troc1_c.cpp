5fa656aa0566f4b24c67e6fd19690350ab429066
Andhika Rahman <andhikarhmnp@gmail.com>
2026-07-11 07:33:24 +0700

    solve(tlx): TROC #1 / C - Naik dan Turun

---
+++ quest-001/troc1_c.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <ios>
+#include <iostream>
+
+typedef long long ll;
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int n, q;
+
+    std::cin >> n >> q;
+
+    ll l = 0, r = 0;
+
+    while(q--) {
+        int t, x, y;
+
+        std::cin >> t >> x >> y;
+
+        if(t == 1) {
+            l += (ll)y;
+            if(x == n) r += (ll)y;
+        }else if(t == 2) {
+            r -= (ll)y;
+            if(x == n) l -= (ll)y;
+        }
+    }
+
+    if(l < 0) l = -l;
+    if(r < 0) r = -r;
+
+    ll ans = std::max(l, r);
+
+    std::cout << ans;
+
+    return 0;
+}
