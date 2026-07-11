3cbc64328eb7acec029a9b62540754c5010b9f73
Andhika Rahman <andhikarhmnp@gmail.com>
2026-07-11 09:13:29 +0700

    solve(tlx): TROC #13 / C - Bilangan dan Operasi

---
+++ quest-001/troc13_c.cpp
@@ -0,0 +1,35 @@
+#include <ios>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int n;
+
+    std::cin >> n;
+
+    std::vector<int> v(n);
+    std::unordered_set<int> us;
+
+    for(int i = 0; i < n; i++) {
+        std::cin >> v[i];
+        us.insert(v[i]);
+    }
+
+    if(us.size() == 1) {
+        std::cout << 1;
+        return 0;
+    }
+
+    if(us.size() == n) {
+        std::cout << n;
+        return 0;
+    }
+
+    std::cout << n - 1;
+
+    return 0;
+}
